52f47c8698cf2e48dd23cae8aa12991322cb4268
Andy_Sun128 <77774627+AndySun128@users.noreply.github.com>
2025-10-23 09:37:11 +0800

    Add files via upload

---
+++ 92.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using std::cout;
+using std::cin;
+
+int main() {
+    int t[6]={100,50,20,10,5,1};
+    int m;cin>>m;
+    for(int i=0;i<6;i++) {
+        cout<<m/t[i]<<" ";
+        m=m%t[i];
+    }
+}
